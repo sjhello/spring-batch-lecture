72085cac536a952baafa76a5a1f9e5520b67158d
sjhello <sjhello235@gmail.com>
2023-06-04 17:07:44 +0900

    develop to main (#6)
    * Add Part2.3 hello spring batch (#1)
    
    * Add Part2.3.2 batch-db-schema (#2)
    
    * Feature/step (#3)
    
    * starter-web, h2 설정
    
    * port 변경
    
    ---------
    
    Co-authored-by: Bailey <bailey@plgrim.com>
    
    * Feature/step (#4)
    
    * starter-web, h2 설정
    
    * port 변경
    
    * commit log test
    
    * job validator (#5)
    
    ---------
    
    Co-authored-by: Bailey <bailey@plgrim.com>

---
+++ src/main/java/io/springbatch/batch/job/validator/ValidatorConfiguration.java
@@ -0,0 +1,56 @@
+package io.springbatch.batch.job.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ValidatorConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .validator(new CustomJobParametersValidator())
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
