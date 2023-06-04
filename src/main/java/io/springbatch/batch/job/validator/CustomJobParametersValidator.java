f0c30478b3b043493f2b2ab4bd97fa537907457f
sjhello <sjhello235@gmail.com>
2023-06-04 17:05:28 +0900

    job validator

---
+++ src/main/java/io/springbatch/batch/job/validator/CustomJobParametersValidator.java
@@ -0,0 +1,16 @@
+package io.springbatch.batch.job.validator;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+
+public class CustomJobParametersValidator implements JobParametersValidator {
+
+    @Override
+    public void validate(JobParameters jobParameters) throws JobParametersInvalidException {
+
+        if (jobParameters.getString("name") == null) {
+            throw new JobParametersInvalidException("name 이 없습니다.");
+        }
+    }
+}
